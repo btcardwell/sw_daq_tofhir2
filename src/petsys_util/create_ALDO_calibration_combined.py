608a5d16ed1755de53c82795acdd3e6c0a77d248
pc-mtd-tb01 <marco.toliman.lucchini@cern.ch>
2021-07-16 08:27:53 +0200

    updates for CERN TB

--- src/petsys_util/create_ALDO_calibration_combined.py
+++ src/petsys_util/create_ALDO_calibration_combined.py
@@ -1,5 +1,5 @@
 list_ASIC = []
-#list_ASIC.append(0)
+list_ASIC.append(0)
 list_ASIC.append(2)
 
 list_ALDO = ['A', 'B']
@@ -8,24 +8,20 @@ map_input = {}
 for asic in list_ASIC:
     map_input[asic] = {}
 
-#map_input[0]['A'] = '../config/ALDO_T2TB04/aldo_scan_ALDO_A_high.tsv'
-#map_input[0]['B'] = '../config/ALDO_T2TB04/aldo_scan_ALDO_B_high.tsv'
-#map_input[2]['A'] = '../config/ALDO_T2TB03_v0/aldo_scan_ALDO_A.tsv'
-#map_input[2]['B'] = '../config/ALDO_T2TB03_v0/aldo_scan_ALDO_B.tsv'
-map_input[2]['A'] = 'ALDO_T2TB03_v0/aldo_scan_ALDO_A_low_RECALIB.tsv'
-map_input[2]['B'] = 'ALDO_T2TB03_v0/aldo_scan_ALDO_B_low_RECALIB.tsv'
+map_input[0]['A'] = '../config/ALDO_T2TB06_2021_07_15/aldo_scan_ALDO_A_low.tsv'
+map_input[0]['B'] = '../config/ALDO_T2TB06_2021_07_15/aldo_scan_ALDO_B_low.tsv'
+map_input[2]['A'] = '../config/ALDO_T2TB03_v0_2021_07_15/aldo_scan_ALDO_A_low.tsv'
+map_input[2]['B'] = '../config/ALDO_T2TB03_v0_2021_07_15/aldo_scan_ALDO_B_low.tsv'
 
 for aldo in list_ALDO:
-    outFileName = 'aldo_scan_ALDO_%s_low_RECALIB.tsv' % aldo
+    outFileName = 'aldo_scan_ALDO_%s_low.tsv' % aldo
     out = open(outFileName, 'w')
     
     for asic in list_ASIC:
         with open(map_input[asic][aldo], 'r') as f:
-            print "ASIC:",asic 
             for line in f:
+                print line
                 readings = line.split()
-                line_out = '0\t0\t%d\t%3d\t%f\n' %(asic,int(readings[3]),float(readings[4]))
-                print line_out 
+                line_out = '0\t0\t%d\t%3d\t%f\n' %(asic,int(readings[0]),float(readings[1]))
                 out.write(line_out)
     out.close()
-                
