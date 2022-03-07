46a5acd4e81f84c75d3b05ba0b73fd8c5320a44c
mtd <mtd@cern.ch>
2022-03-07 17:39:36 +0100

    script to combine calibration files

--- src/raw_data/DAQv1Reader.cpp
+++ src/raw_data/DAQv1Reader.cpp
@@ -21,41 +21,41 @@ using namespace PETSYS;
 static unsigned ELINK_TO_CHIPID(unsigned n)
 {
        switch (n) {
-                case 6: return 0;
-                case 7: return 1;
+                case 6: return 4;
+                case 7: return 5;
 
-                case 9: return 4;
-                case 8: return 5;
+                case 9: return 2;
+                case 8: return 3;
 
-                case 17: return 4;
-                case 16: return 5;
+                case 17: return 0;
+                case 16: return 1;
 
-                case 10: return 4;
-                case 11: return 5;
+                case 10: return 6;
+                case 11: return 7;
 
-                case 18: return 4;
-                case 21: return 5;
+                case 18: return 8;
+                case 21: return 9;
 
-                case 20: return 4;
-                case 19: return 5;
+                case 20: return 10;
+                case 19: return 11;
 
-                case 5: return 4;
-                case 4: return 5;
+                case 5: return 12;
+                case 4: return 13;
 
-                case 3: return 4;
-                case 2: return 5;
+                case 3: return 14;
+                case 2: return 15;
 
-                case 27: return 4;
-                case 26: return 5;
+                case 27: return 16;
+                case 26: return 17;
 
-                case 1: return 4;
-                case 0: return 5;
+                case 1: return 18;
+                case 0: return 19;
 
-                case 22: return 4;
-                case 23: return 5;
+                case 22: return 20;
+                case 23: return 21;
 
-                case 25: return 4;
-                case 24: return 5;
+                case 25: return 22;
+                case 24: return 23;
 		  
 		default: return -1;
 	}
