eecaa667a7e923914402b6cc58f342ae5f521720
Anthony LaTorre <alatorre@caltech.edu>
2022-12-15 12:17:31 -0500

    update

--- src/petsys_util/convert.py
+++ src/petsys_util/convert.py
@@ -1,51 +1,58 @@
 #!/usr/bin/env python
-
+from __future__ import print_function
 import os
 import argparse
 
 parser = argparse.ArgumentParser(description='convert raw data')
 parser.add_argument("--config", type=str, required=True, help="Configuration file")
-parser.add_argument("-r", type=str, dest="run", required=True, help = "Run number")
+parser.add_argument("-r", "--run", type=str, required=True, help="Run number")
 parser.add_argument("--mode", type=str, required=True, choices=["r", "s", "c", "e"], help = "reconstruction mode (singles, coincicences or event)")
 parser.add_argument("--refChannel", type=str, required=False, help = "reference channel")
 parser.add_argument("--pedestals", dest="pedestals", action="store_true", help="Enable the acquisition of pedestals")
-
+parser.add_argument("--data-dir", default="/storage/TOFHIR2", help="Directory to store raw and reco data")
 args = parser.parse_args()
 
+if not os.path.exists(args.data_dir):
+    print("Error: data directory '%s' does not exist!" % args.data_dir)
+    print("Please rerun with --data-dir specifying where output data is")
+
+raw_prefix = os.path.join(args.data_dir,"raw",args.run)
+reco_prefix = os.path.join(args.data_dir,"reco","%s" % args.run)
+
 if args.mode == 'r':
-    command = "./convert_raw_to_raw --config "+args.config+" -i /home/aang/tofhir-data/TOFHIR2/raw/"+args.run+" -o /home/aang/tofhir-data/TOFHIR2/reco/"+args.run+"_r.root"
+    command = "./convert_raw_to_raw --config %s -i %s -o %s_r.root" % (args.config, raw_prefix, reco_prefix)
     os.system(command)
 
 if args.mode == 's':
-    mainCommand = "./convert_raw_to_singles --config "+args.config+" -i /home/aang/tofhir-data/TOFHIR2/raw/"+args.run+" --writeRoot"
+    mainCommand = "./convert_raw_to_singles --config %s -i %s --writeRoot" % (args.config, raw_prefix)
     if args.pedestals:
-        command = "./convert_raw_to_singles --config "+args.config+" -i /home/aang/tofhir-data/TOFHIR2/raw/"+args.run+"_ped1 -o /home/aang/tofhir-data/TOFHIR2/reco/"+args.run+"_ped1_s.root --writeRoot"
+        command = "./convert_raw_to_singles --config %s -i %s_ped1 -o %s_ped1_s.root --writeRoot" % (args.config, raw_prefix, reco_prefix)
         os.system(command)
-        command = "./convert_raw_to_singles --config "+args.config+" -i /home/aang/tofhir-data/TOFHIR2/raw/"+args.run+"_ped2 -o /home/aang/tofhir-data/TOFHIR2/reco/"+args.run+"_ped2_s.root --writeRoot"
+        command = "./convert_raw_to_singles --config %s -i %s_ped2 -o %s_ped2_s.root --writeRoot" % (args.config, raw_prefix, reco_prefix)
         os.system(command)
-        command = "./analyze_pedestals.exe /home/aang/tofhir-data/TOFHIR2/reco/"+args.run+"_ped1_s.root /home/aang/tofhir-data/TOFHIR2/reco/"+args.run+"_ped2_s.root /home/aang/tofhir-data/TOFHIR2/reco/"+args.run+"_pedestals.root"
+        command = "./analyze_pedestals.exe %s_ped1_s.root %s_ped2_s.root %s_pedestals.root" % (raw_prefix, raw_prefix, reco_prefix)
         os.system(command)
-        mainCommand += " --pedestals -o /home/aang/tofhir-data/TOFHIR2/reco/"+args.run+"_ped_s.root"
+        mainCommand += " --pedestals -o %s_ped_s.root" % reco_prefix
     else:
-        mainCommand += " -o /home/aang/tofhir-data/TOFHIR2/reco/"+args.run+"_s.root"
+        mainCommand += " -o %s_s.root" % reco_prefix
     os.system(mainCommand)
 
 if args.mode == 'c':
-    command = "./convert_raw_to_coincidence --config "+args.config+" -i /home/aang/tofhir-data/TOFHIR2/raw/"+args.run+" -o /home/aang/tofhir-data/TOFHIR2/reco/"+args.run+"_c.root --writeRoot"
+    command = "./convert_raw_to_coincidence --config %s -i %s -o %s_c.root --writeRoot" % (args.config, raw_prefix, reco_prefix)
     os.system(command)
 
 if args.mode == 'e':
-    mainCommand = "./convert_raw_to_event --config "+args.config+" -i /home/aang/tofhir-data/TOFHIR2/raw/"+args.run+" --writeRoot"
+    mainCommand = "./convert_raw_to_event --config %s -i %s --writeRoot" % (args.config, raw_prefix)
     if args.pedestals:
-        command = "./convert_raw_to_singles --config "+args.config+" -i /home/aang/tofhir-data/TOFHIR2/raw/"+args.run+"_ped1 -o /home/aang/tofhir-data/TOFHIR2/reco/"+args.run+"_ped1_s.root --writeRoot"
+        command = "./convert_raw_to_singles --config %s -i %s_ped1 -o %s_ped1_s.root --writeRoot" % (args.config, raw_prefix, reco_prefix)
         os.system(command)
-        command = "./convert_raw_to_singles --config "+args.config+" -i /home/aang/tofhir-data/TOFHIR2/raw/"+args.run+"_ped2 -o /home/aang/tofhir-data/TOFHIR2/reco/"+args.run+"_ped2_s.root --writeRoot"
+        command = "./convert_raw_to_singles --config %s -i %s_ped2 -o %s_ped2_s.root --writeRoot" % (args.config, raw_prefix, reco_prefix)
         os.system(command)
-        command = "./analyze_pedestals.exe /home/aang/tofhir-data/TOFHIR2/reco/"+args.run+"_ped1_s.root /home/aang/tofhir-data/TOFHIR2/reco/"+args.run+"_ped2_s.root /home/aang/tofhir-data/TOFHIR2/reco/"+args.run+"_pedestals.root"
+        command = "./analyze_pedestals.exe %s_ped1_s.root %s_ped2_s.root %s_pedestals.root" % (raw_prefix, raw_prefix, reco_prefix)
         os.system(command)
-        mainCommand += " --pedestals -o /home/aang/tofhir-data/TOFHIR2/reco/"+args.run+"_ped_e.root"
+        mainCommand += " --pedestals -o %s_ped_e.root" % reco_prefix
     else:
-        mainCommand += " -o /home/aang/tofhir-data/TOFHIR2/reco/"+args.run+"_e.root"
+        mainCommand += " -o %s_e.root" % reco_prefix
     if args.refChannel :
-        mainCommand += " --refChannel "+args.refChannel
+        mainCommand += " --refChannel %s" % args.refChannel
     os.system(mainCommand)
